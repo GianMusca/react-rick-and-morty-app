0dce892a8ac69a599b4cb9d9fa2683cbad289c61
GianMusca <gmuscariello@itba.edu.ar>
2024-12-02 14:38:03 -0300

    feat(elements): add new Home page draft

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+export default function Home() {
+    return(
+        <div className='d-flex justify-content-center align-items-center'>
+            <div className='container'>
+                <h1 className='row'>Project Rick & Morty</h1>
+            </div>
+        </div>
+    )
+}
