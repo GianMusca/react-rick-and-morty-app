8924ef507c5daf1754082317d87ca2e8e6f273f1
GianMusca <gmuscariello@itba.edu.ar>
2024-12-03 18:52:31 -0300

    style: add principal colors and typography

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import './Home.css'
+import '../../index.css'
 
 export default function Home() {
     return(
-        <div className='d-flex justify-content-center align-items-center'>
+        <section id="home-page" className='d-flex justify-content-center align-items-center'>
             <div className='container'>
                 <h1 className='row'>Project Rick & Morty</h1>
             </div>
-        </div>
+        </section>
     )
 }
